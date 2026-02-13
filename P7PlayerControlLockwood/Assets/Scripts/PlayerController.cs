7377666c1963f46b58f271c28c21f60a14fe40ed
731096 <731096@student.dusd.net>
2026-02-13 15:25:34 -0800

    challenge 1
    strated it

--- P7PlayerControlLockwood/Assets/Scripts/PlayerController.cs
+++ P7PlayerControlLockwood/Assets/Scripts/PlayerController.cs
@@ -23,7 +23,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         if (Input.GetKeyDown(switchKey)) 
-        { 
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+
         }
     }
 }
