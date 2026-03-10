fcdca149c588fc11bb129c06df58d56f43158fd4
731096 <731096@student.dusd.net>
2026-03-10 14:28:48 -0700

    getcomponent
    done

--- P7PlayerControlLockwood/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ P7PlayerControlLockwood/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -6,7 +6,7 @@ public class PlayerControllerX : MonoBehaviour
 {
     public float speed;
     public float rotationSpeed;
-    public float verticalInput;
+    public float VerticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,13 +18,13 @@ public class PlayerControllerX : MonoBehaviour
     void FixedUpdate()
     {
         // get the user's vertical input
-        verticalInput = Input .GetAxis("vertical");
+        VerticalInput = Input .GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * VerticalInput);
 
     }
 }
--- P7PlayerControlLockwood/Assets/Challenge 1/Scripts/SpinPropeller.cs
+++ P7PlayerControlLockwood/Assets/Challenge 1/Scripts/SpinPropeller.cs
@@ -1,7 +1,8 @@
 using UnityEngine;
 
 public class SpinPropeller : MonoBehaviour
-{
+{ 
+   public float rotatSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
